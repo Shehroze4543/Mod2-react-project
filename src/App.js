5b32bd73f9df5db5e31a1c5c59b6140e85f0cc5b
Shehroze4543 <86188771+Shehroze4543@users.noreply.github.com>
2023-08-03 21:48:02 -0400

    third Commit of the day

--- src/App.js
+++ src/App.js
@@ -4,44 +4,98 @@ import axios from "axios";
 
 function App() {
   const [question, setQuestion] = useState([]);
+  const [count, setCount] = useState(0);
   const [ansA, setAnswerA] = useState("");
   const [ansB, setAnswerB] = useState("");
   const [ansC, setAnswerC] = useState("");
   const [ansD, setAnswerD] = useState("");
+  const [correctAns, setCorrectAns] = useState("");
   const apiKey = "ctbYfQVwD7PYZSlHpr04pv8YRZnPWRA6Wl4CEvlB";
   const limit = 10;
   const category = "JavaScript";
   function getData(e) {
     e.preventDefault();
+    setCount(() => count + 1);
     axios
       .get(
         `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=${limit}&tags=${category}`
       )
       .then((response) => {
-        // console.log(response.data[0]);
+        console.log(response.data[0]);
         // setData(console.log(response.data[0]));
         // setData(console.log(response.data[0].question));
-        // setAnswer(console.log(response.data[0].answers.answer_a));
-        // setAnswer(console.log(response.data[0].answers.answer_b));
-        // setAnswer(console.log(response.data[0].answers.answer_c));
-        // setAnswer(console.log(response.data[0].answers.answer_d));
         setQuestion(response.data[0].question);
         setAnswerA(response.data[0].answers.answer_a);
         setAnswerB(response.data[0].answers.answer_b);
         setAnswerC(response.data[0].answers.answer_c);
         setAnswerD(response.data[0].answers.answer_d);
+        setCorrectAns(response.data[0].correct_answers);
+        // console.log(response.data[0].correct_answers.answer_a_correct);
+        // console.log(response.data[0].correct_answers.answer_b_correct);
+        // console.log(response.data[0].correct_answers.answer_c_correct);
+        // console.log(response.data[0].correct_answers.answer_d_correct);
       });
+  }
+  function checkAnswerA() {
+    if (Object.values(correctAns)[0] === "true") {
+      console.log(`Sahi Jawab`);
+    } else {
+      console.log("Ghalt Jawab");
+    }
+  }
+  function checkAnswerB() {
+    if (Object.values(correctAns)[1] === "true") {
+      console.log(`Sahi Jawab`);
+    } else {
+      console.log("Ghalt Jawab");
+    }
+  }
+  function checkAnswerC() {
+    if (Object.values(correctAns)[2] === "true") {
+      console.log(`Sahi Jawab`);
+    } else {
+      console.log("Ghalt Jawab");
+    }
+  }
+  function checkAnswerD() {
+    if (Object.values(correctAns)[3] === "true") {
+      console.log(`Sahi Jawab`);
+    } else {
+      console.log("Ghalt Jawab");
+    }
   }
   return (
     <>
-      <h3>{question}</h3>
+      <h3>
+        Question #{count}: {question}
+      </h3>
       <ul>
-        <li>{ansA}</li>
-        <li>{ansB}</li>
-        {ansC === null ? null : <li>{ansC}</li>}
-        {ansD === null ? null : <li>{ansD}</li>}
+        <li>
+          <button onClick={checkAnswerA}>{ansA}</button>
+        </li>
+        <li>
+          <button onClick={checkAnswerB}>{ansB}</button>
+        </li>
+        {ansC === null ? null : (
+          <li>
+            <button onClick={checkAnswerC}>{ansC}</button>
+          </li>
+        )}
+        {ansD === null ? null : (
+          <li>
+            <button onClick={checkAnswerD}>{ansD}</button>
+          </li>
+        )}
       </ul>
       <button onClick={getData}>Click me</button>
+      <ul>
+        {Object.entries(correctAns).map(([key, val]) => (
+          <li key={key}>
+            <strong>{key}</strong>: '{val}'
+          </li>
+        ))}
+        {/* {console.log(Object.values(correctAns)[0])} */}
+      </ul>
     </>
   );
 }
